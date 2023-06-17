9759ec90354b7bdf02b3afec03ae3b2d2a1b883a
TeddyTjoe1990 <her21059@byui.edu>
2023-06-17 23:12:55 +0700

    made some updates

--- L04-team/swagger.js
+++ L04-team/swagger.js
@@ -1,25 +1,21 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-	info: {
-		title: 'My API',
-		description: 'Description',
-	},
-	host: 'localhost:8080',
-	schemes: ['http'],
+  info: {
+    title: 'My API',
+    description: 'Temple API',
+  },
+  host: 'cse341-',
+  schemes: ['http'],
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = ['./routes/temple.js'];
 
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
-
-// Generates swagger.json file
+// generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
 
 // Run server after it gets generated
 // swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-// 	await import('./index.js');
+//   await import('./index.js');
 // });
